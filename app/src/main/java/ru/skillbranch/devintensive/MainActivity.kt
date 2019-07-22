a46130538138edf81c407c040e639906bfa0cc48
Свэй <wss@inbox.ru>
2019-07-23 00:32:59 +0800

    hometask_3

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState?.putString("STATUS", benderObj.status.name)
-        outState?.putString("QUESTION", benderObj.question.question)
+        outState?.putString("QUESTION", benderObj.question.name)
 
     }
 }
